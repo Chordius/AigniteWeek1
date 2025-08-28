e3071d1ee7eace614c38218cf3b623fc561498bc
Chordius <malau.jesaya@gmail.com>
2025-08-28 18:43:08 +0700

    new file enter.png and renaming

--- src/App.jsx
+++ src/App.jsx
@@ -25,24 +25,10 @@ function App() {
           <img src={logo} className="h-12 pt-2" alt="logo" />
           <div className='bg-[#303030] p-4 rounded-2xl mb-8'>
             <p>
-              Next Steps<br/>
-              <br/>
-              Sketch Your Module Outline<br/>
-              Start with an intro lesson, then follow with HTML structure → styling → layout → responsive design → tools → project work.<br/>
-              <br/>
-              Embed Resources Strategically<br/>
-              Include links to the Odin overview, Noble Desktop syllabus, UCSD course, Coursera labs, and MDN documentation at appropriate places within the content.<br/>
-              <br/>
-              Create Supporting Materials<br/>
-              <br/>
-              Slides or reading content explaining each concept.<br/>
-              <br/>
-              Step-by-step exercises, quizzes, and a final mini-project.<br/>
-              <br/>
-              Provide external resource links for deeper exploration and reference.<br/>
-              <br/>
-              Add Interactivity<br/>
-              Consider embedding code pens, live coding exercises, or referring learners to interactive platforms like Khan Academy.<br/>
+              That is not a schedule.<br/>
+              Date:  Why did the<br/>
+              Time:  Chicken<br/>
+              Event: Cross the Road<br/>
             </p>
           </div>
         </div>
