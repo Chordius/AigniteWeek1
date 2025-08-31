afaeaab4087dd076a4a7913bfe5dae34d1bc7435
Chordius <malau.jesaya@gmail.com>
2025-08-31 15:49:50 +0700

    revised

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import enter from './enter.svg';
 import './App.css';
 
@@ -6,30 +5,37 @@ function App() {
   return (
     <div className="flex justify-center w-full min-h-screen bg-[#212121] text-white">
       <header className="absolute top-0 text-xl p-5 bg-[#303030] w-full text-center rounded-lg shadow-sm">
-        To-Do List Chatbot
+        To-Do List App
       </header>
-      <main className="pt-36 w-1/2">
+      <main className="pt-36 w-3/4">
         {/* User Prompt */}
-        <div className='flex justify-end'>
-          <div className='bg-[#303030] p-4 rounded-2xl max-w-3/4'>
-            <p>GPT, why did the chicken cross the road?</p>
+        <div className='flex justify-center'>
+          <div className='bg-[#303030] p-4 rounded-2xl w-3/4 shadow-md'>
+            <p>Gemini, why did the chicken cross the road at 10 PM?</p>
           </div>
           <img src={enter} className="pl-2 h-12 pt-2 scale-95" alt="enter" />
         </div>
 
         {/* Spacing */}
-        <div className='p-4'/>
+        <div className='p-6'/>
 
-        {/* Chatbot Response */}
-        <div className='flex justify-start'>
-          <img src={logo} className="h-12 pt-2" alt="logo" />
-          <div className='bg-[#303030] p-4 rounded-2xl mb-8'>
-            <p>
-              That is not a schedule.<br/>
-              Date:  Why did the<br/>
-              Time:  Chicken<br/>
-              Event: Cross the Road<br/>
-            </p>
+        {/* To-Do List */}
+        <div className='flex justify-center'>
+          <div className='w-[85%] flex flex-col gap-y-3'>
+            <p className='font-semibold text-xl'>📋 Your To-Do List</p>
+            <hr/>
+
+            {/* Card */}
+            <div className='bg-[#303030] p-4 rounded-2xl mb-8 shadow-lg'>
+              <p className='text-base'>
+                <span className='text-xl font-semibold'>Crossing the Road</span><br/>
+                <br/>
+                Why did the Chicken Cross the Road?<br/>
+                Date:  30 Februari 2025, 10:00 PM<br/>
+                Status:  Active<br/>
+              </p>
+            </div>
+
           </div>
         </div>
       </main>
