56311d26c917eb28a0144ed8b126533cdd0241c5
Tianrider <hadiwijayachristian7@gmail.com>
2025-08-31 22:10:03 +0700

    Add Prettier configuration and refactor App component for consistent styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,70 @@
-import enter from "./enter.svg";
-import "./App.css";
+import enter from './enter.svg';
+import './App.css';
 
 function App() {
-	return (
-		<div className="flex justify-center w-full min-h-screen bg-[#212121] text-white">
-			<header className="absolute top-0 text-xl p-5 bg-[#303030] w-full text-center rounded-lg shadow-sm">
-				To-Do List App
-			</header>
-			<main className="pt-36 w-3/4">
-				{/* User Prompt */}
-				<div className="flex justify-center">
-					<input
-						className="bg-[#303030] p-4 rounded-2xl w-3/4 shadow-md"
-						placeholder="Type your task here..."
-					></input>
-					<button className="pl-2 h-12 pt-2 scale-95">
-						<img
-							src={enter}
-							alt="enter"
-							className="w-full h-full"
-						/>
-					</button>
-				</div>
+  return (
+    <div className="flex justify-center w-full min-h-screen bg-[#212121] text-white">
+      <header className="absolute top-0 text-xl p-5 bg-[#303030] w-full text-center rounded-lg shadow-sm">
+        To-Do List App
+      </header>
+      <main className="pt-36 w-3/4">
+        {/* User Prompt */}
+        <div className="flex justify-center">
+          <input className="bg-[#303030] p-4 rounded-2xl w-3/4 shadow-md" placeholder="Type your task here..."></input>
+          <button className="pl-2 h-12 pt-2 scale-95">
+            <img src={enter} alt="enter" className="w-full h-full" />
+          </button>
+        </div>
 
-				{/* Spacing */}
-				<div className="p-6" />
+        {/* Spacing */}
+        <div className="p-6" />
 
-				{/* To-Do List */}
-				<div className="flex justify-center">
-					<div className="w-[85%] flex flex-col gap-y-4">
-						<p className="font-semibold text-xl">
-							📋 Your To-Do List
-						</p>
-						<hr />
+        {/* To-Do List */}
+        <div className="flex justify-center">
+          <div className="w-[85%] flex flex-col gap-y-4">
+            <p className="font-semibold text-xl">📋 Your To-Do List</p>
+            <hr />
 
-						{/* Card 1*/}
-						<div className="bg-[#303030] p-4 rounded-2xl shadow-lg">
-							<p className="text-base">
-								<span className="text-xl font-semibold">
-									Crossing the Road
-								</span>
-								<br />
-								<br />
-								Why did the Chicken Cross the Road?
-								<br />
-								Date: 30 Februari 2025,
-								<br />
-								Time: 11:00 PM
-								<br />
-								Status: Active
-								<br />
-							</p>
-							<input type="checkbox" />
-						</div>
+            {/* Card 1*/}
+            <div className="bg-[#303030] p-4 rounded-2xl shadow-lg">
+              <p className="text-base">
+                <span className="text-xl font-semibold">Crossing the Road</span>
+                <br />
+                <br />
+                Why did the Chicken Cross the Road?
+                <br />
+                Date: 30 Februari 2025,
+                <br />
+                Time: 11:00 PM
+                <br />
+                Status: Active
+                <br />
+              </p>
+              <input type="checkbox" />
+            </div>
 
-						{/* Card 2*/}
-						<div className="bg-[#303030] p-4 rounded-2xl shadow-lg">
-							<p className="text-base">
-								<span className="text-xl font-semibold">
-									Cleaning the House
-								</span>
-								<br />
-								<br />
-								Why did the Chicken clean the House?
-								<br />
-								Date: 30 Februari 2025
-								<br />
-								Time: 10:00 PM
-								<br />
-								Status: Active
-								<br />
-							</p>
-							<input type="checkbox" />
-						</div>
-					</div>
-				</div>
-			</main>
-		</div>
-	);
+            {/* Card 2*/}
+            <div className="bg-[#303030] p-4 rounded-2xl shadow-lg">
+              <p className="text-base">
+                <span className="text-xl font-semibold">Cleaning the House</span>
+                <br />
+                <br />
+                Why did the Chicken clean the House?
+                <br />
+                Date: 30 Februari 2025
+                <br />
+                Time: 10:00 PM
+                <br />
+                Status: Active
+                <br />
+              </p>
+              <input type="checkbox" />
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
